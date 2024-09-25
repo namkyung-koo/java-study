a28427709d80df877c4e7a2fd36f29f814fa3c17
Namkyung Koo <united94@naver.com>
2024-09-25 17:38:04 +0900

    fix : bug in Account class

--- java-basic/src/oop1/ex/Rectangle.java
+++ java-basic/src/oop1/ex/Rectangle.java
@@ -1,18 +1,15 @@
 package oop1.ex;
 
 public class Rectangle {
-    int width = 5;
-    int height = 8;
-    int area = calculateArea();
-    int perimeter = calculatePerimeter();
-    boolean square = isSquare();
+    int width;
+    int height;
 
     int calculateArea() {
         return width * height;
     }
 
     int calculatePerimeter() {
-        return 2 * (width * height);
+        return 2 * (width + height);
     }
 
     boolean isSquare() {
--- java-basic/src/oop1/ex/RetangleOopMain.java
+++ java-basic/src/oop1/ex/RetangleOopMain.java
@@ -4,9 +4,16 @@ public class RetangleOopMain {
 
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle();
+        rectangle.width = 8;
+        rectangle.height = 8;
 
-        System.out.println("넓이: " + rectangle.area);
-        System.out.println("둘레 길이: " + rectangle.perimeter);
-        System.out.println("정사각형 여부: " + rectangle.square);
+        int area = rectangle.calculateArea();
+        System.out.println("넓이: " + area);
+
+        int perimeter = rectangle.calculatePerimeter();
+        System.out.println("둘레 길이: " + perimeter);
+
+        boolean square = rectangle.isSquare();
+        System.out.println("정사각형 여부: " + square);
     }
 }
