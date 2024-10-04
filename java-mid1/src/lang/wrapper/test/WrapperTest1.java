f212ca8cdb4bb0a7dc17f0e89f23070a32958698
Namkyung Koo <united94@naver.com>
2024-10-04 18:17:46 +0900

    chore : add wrapper, clazz, system, math packages

---
+++ java-mid1/src/lang/wrapper/test/WrapperTest1.java
@@ -0,0 +1,12 @@
+package lang.wrapper.test;
+
+public class WrapperTest1 {
+
+    public static void main(String[] args) {
+        String str1 = "10";
+        String str2 = "20";
+
+        System.out.print("두 수의 합: ");
+        System.out.println(Integer.parseInt(str1) + Integer.parseInt(str2));
+    }
+}
