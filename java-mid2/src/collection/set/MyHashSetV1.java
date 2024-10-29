02b7ff092ac37af7541bff6aac6a717f03bcf15c
Namkyung Koo <united94@naver.com>
2024-10-29 20:40:15 +0900

    chore: add files and member package in collection.set

---
+++ java-mid2/src/collection/set/MyHashSetV1.java
@@ -0,0 +1,77 @@
+package collection.set;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class MyHashSetV1 {
+
+    static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    LinkedList<Integer>[] buckets;
+
+    private int size = 0;
+    private int capacity = DEFAULT_INITIAL_CAPACITY;
+
+    public MyHashSetV1() {
+        initBuckets();
+    }
+
+    public MyHashSetV1(int capacity) {
+        this.capacity = capacity;
+        initBuckets();
+    }
+
+    private void initBuckets() {
+        buckets = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    public boolean add(int value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Integer> bucket = buckets[hashIndex];
+        if (bucket.contains(value)) {
+            return false;
+        }
+
+        bucket.add(value);
+        size++;
+        return true;
+    }
+
+    public boolean contains(int searchValue) {
+        int hashIndex = hashIndex(searchValue);
+        LinkedList<Integer> bucket = buckets[hashIndex];
+        return bucket.contains(searchValue);
+    }
+
+    public boolean remove(int value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Integer> bucket = buckets[hashIndex];
+        boolean result = bucket.remove(Integer.valueOf(value));// 주의 !! 기본형을 인자로 전달하면 해당 인덱스의 값을 지워버린다.
+        if (result) {
+            size--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private int hashIndex(int value) {
+        return value % capacity;
+    }
+
+    private int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV1{" +
+                "buckets=" + Arrays.toString(buckets) +
+                ", size=" + size +
+                ", capacity=" + capacity +
+                '}';
+    }
+}
