eed9991bcab0bdea722e7a1b498d795d3b88c34e
Namkyung Koo <united94@naver.com>
2024-10-30 20:42:04 +0900

    chore : add javaset package in collection.set

---
+++ java-mid2/src/collection/set/javaset/JavaSetMain.java
@@ -0,0 +1,35 @@
+package collection.set.javaset;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class JavaSetMain {
+
+    public static void main(String[] args) {
+        Set<String> hashSet = new HashSet<>();
+        Set<String> linkedHashSet = new LinkedHashSet<>();
+        Set<String> treeSet = new TreeSet<>();
+
+        run(new HashSet<>());
+        run(new LinkedHashSet<>());
+        run(new TreeSet<>());
+    }
+
+    private static void run(Set<String> set) {
+        System.out.println("set = " + set.getClass());
+        set.add("C");
+        set.add("B");
+        set.add("A");
+        set.add("1");
+        set.add("2");
+
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+    }
+}
