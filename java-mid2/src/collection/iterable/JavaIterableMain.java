019095ab8bd1c3f44a8c1f68e6cf429a14b5face
Namkyung Koo <united94@naver.com>
2024-11-02 14:16:39 +0900

    chore: add compare, iterable, utils packages in collection

---
+++ java-mid2/src/collection/iterable/JavaIterableMain.java
@@ -0,0 +1,42 @@
+package collection.iterable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class JavaIterableMain {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+        printAll(list.iterator());
+        printAll(set.iterator());
+        System.out.println();
+
+        foreach(list);
+        foreach(set);
+
+    }
+
+    private static void foreach(Iterable<Integer> iterable) {
+        System.out.println("iterable = " + iterable.getClass());
+        for (Integer i : iterable) {
+            System.out.println(i);
+        }
+    }
+
+    private static void printAll(Iterator<Integer> iterator) {
+        System.out.println("iterator = " + iterator.getClass());
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
